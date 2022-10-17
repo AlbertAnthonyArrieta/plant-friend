4bfe86e0dbfc1380b2a99ccecc419a3ada3bf7d4
Arrieta <albert.arrieta@uleth.ca>
2022-10-17 11:38:55 -0600

    initial commit

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,3 +5,17 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+// by default:
+// - Plant starts healthy
+// - dry out over time
+// - deplete in nutrients over time
+
+// interactions:
+// - water it, replenish the plant
+// - feed it, nutruents for the plant
+// - trim it
+
+// care:
+// too much water, plant near death state
+// - too much fertilizer
+// - if the plant falls into a near death state, you can only heal it by trimming it.
